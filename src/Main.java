9fbdf7a999a604a2149811b69b03836e06dea7d8
oliverd2 <161394760+oliverd2@users.noreply.github.com>
2025-08-13 12:08:08 +1000

    Made Grid

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,13 @@ public class Main extends JFrame {
 
       @Override
       public void paint(Graphics g) {
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
+        g.setColor(java.awt.Color.BLACK);
+        g.drawRect(10, 10, 700, 700);
+        
+        for (int i = 10; i < 720; i+= 35) {
+          g.drawLine(i, 10, i, 710);
+          g.drawLine(10, i, 710, i);
+        }
       }
     }
 
