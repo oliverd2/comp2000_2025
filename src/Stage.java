382754077c43787fbfd0b701f57439790fc379eb
oliverd2 <161394760+oliverd2@users.noreply.github.com>
2025-09-03 14:50:39 +1000

    Finished work

--- src/Stage.java
+++ src/Stage.java
@@ -11,16 +11,18 @@ public class Stage {
 
   public Stage() {
     grid = new Grid();
-    actors = new ArrayList<Actor>();
-    actors.add(new Cat(grid.cellAtColRow(0, 0).get()));
-    actors.add(new Dog(grid.cellAtColRow(0, 15).get()));
-    actors.add(new Bird(grid.cellAtColRow(12, 9).get()));    
+    actors = new ArrayList<Actor>(); 
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    for(Actor a: actors) {
-      a.paint(g);
+    if (actors.size() == 0) {
+      g.setColor(Color.DARK_GRAY);
+      g.drawString("No actors!", 740, 50);
+    } else {
+      for(Actor a: actors) {
+        a.paint(g);
+      }
     }
     Optional<Cell> underMouse = grid.cellAtPoint(mouseLoc);
     if(underMouse.isPresent()) {
--- src/StageReader.java
+++ src/StageReader.java
@@ -6,7 +6,32 @@ import java.util.List;
 public class StageReader {
   public static Stage readStage(String path) throws IOException {
     Stage stage = new Stage();
-    List<String> lines = Files.readAllLines(Paths.get(path));
-    return new Stage();
+    List<String> lines;
+    try {
+      lines = Files.readAllLines(Paths.get(path));
+    } catch (IOException e) {
+      throw new IOException("Failed to read stage file: " + e.getMessage());
+    } 
+
+    for (String line : lines) {
+      String[] parts = line.split("=");
+      int l = (int)parts[0].charAt(0) - (int)'A';
+      int r = Integer.parseInt(parts[0].substring(1));
+      Cell cell = new Cell(parts[0].charAt(0), r, l * 35 + 10, r * 35 + 10);
+      if(parts[1].toLowerCase().equals("cat")) {
+        stage.actors.add(new Cat(cell));
+        continue;
+      } else if(parts[1].toLowerCase().equals("dog")) {
+        stage.actors.add(new Dog(cell));
+        continue;
+      } else if(parts[1].toLowerCase().equals("bird")) {
+        stage.actors.add(new Bird(cell));
+        continue;
+      } else {
+        throw new IOException("Unknown actor type: " + parts[1]);
+      }
+    }
+
+    return stage;
   }
 }
